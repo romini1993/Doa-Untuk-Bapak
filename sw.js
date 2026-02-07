2c921497ed2f732c6c27b9906dc3d140f2373f43
romini1993 <83305908+romini1993@users.noreply.github.com>
2026-02-07 08:51:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('doa-v1').then((cache) => cache.addAll(['index.html', 'manifest.json']))
-  );
+const CACHE_NAME = 'doa-bapak-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
