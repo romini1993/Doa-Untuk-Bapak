87c769e06adc8d9ff264d31d94ac26ea9b990d0e
romini1993 <83305908+romini1993@users.noreply.github.com>
2026-02-07 09:13:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
 const CACHE_NAME = 'doa-bapak-v1';
-const assets = ['./', './index.html', './manifest.json'];
+// Link icon dari Flaticon harus masuk sini juga
+const assets = [
+  './', 
+  './index.html', 
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/128/15044/15044805.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      // Pake return biar proses install-nya bener-bener kelar
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
